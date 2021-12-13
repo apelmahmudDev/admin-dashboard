ba842db49a66a49ac67b3ad5869530e84caea31c
Apel Mahmud <apelmahmud.dev@gmail.com>
2021-12-13 12:11:12 +0600

    Dashboard ready for continue

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from "react";
+import AdminLayout from "./components/AdminLayout";
 
 function App() {
 	return (
 		<div>
 			<h1>Admin dashboard</h1>
+			<AdminLayout />
 		</div>
 	);
 }
